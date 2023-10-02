eeaf3fcd0f168332f6bded7adef57adab290234a
Maximilian Petkov <max@stan.vision>
2023-10-02 09:40:50 +0300

    Animation Updated

--- main.js
+++ main.js
@@ -1,68 +1,76 @@
-// setTypewriter();
-showChats();
+showDialogues();
 
-function showChats() {
+function showDialogues() {
     const container = document.querySelectorAll(".chats__container");
+
     const topChat = document.querySelector(".chat[data-chat='1']");
     const middleChat = document.querySelector(".chat[data-chat='2']");
     const bottomChat = document.querySelector(".chat[data-chat='3']");
+
+    topChat.setAttribute("data-text", topChat.textContent.trim());
+    topChat.style.minHeight = topChat.scrollHeight + "px";
+    middleChat.setAttribute("data-text", middleChat.textContent.trim());
+    middleChat.style.minHeight = middleChat.scrollHeight + "px";
+    bottomChat.setAttribute("data-text", bottomChat.textContent.trim());
+    bottomChat.style.minHeight = bottomChat.scrollHeight + "px";
+
+    const dialogueOne = new Typewriter(topChat, { delay: 17});
+    const dialogueTwo = new Typewriter(middleChat, { delay: 10});
+    const dialogueThree = new Typewriter(bottomChat, { delay: 17});
     const hideTypewriterCursor = (chat) => chat.querySelector(".Typewriter__cursor").classList.add("hide");
+
     const tl = gsap.timeline({
         repeat: -1,
         defaults: {
-            duration: 1.5,
+            duration: 1.2,
             ease: Power3.easeInOut
         },
     });
     
     tl
-    .fromTo(container, { y: "30%"}, {y: "20%"})
+    .fromTo(container, { y: "30%", autoAlpha: 1, scale: 1}, {y: "20%"})
     .fromTo(topChat, {
         autoAlpha: 0
     }, {
         autoAlpha: 1, 
-        onComplete: () => {
-            console.log("Completed...");
-            // tl.pause();
-        },
-        onStart: () => {
-            console.log("Started...");
-            startTypeWriter(tl,{chat: topChat, speed: 17, pause: 300 });
-            // tl.pause();
-        }, 
+        onComplete: () => tl.pause(),
+        onStart: () => startTypeWriter(tl,{dialogue: dialogueOne, chat: topChat, speed: 17, pause: 300 }), 
        }, "<")
-    // .fromTo(container, {y: "20%"}, {y: "10%"})
-    // .fromTo(middleChat, {
-    //     autoAlpha: 0
-    // }, {
-    //     autoAlpha: 1, 
-    //     onComplete: () => tl.pause(),
-    //     onStart: () => startTypeWriter(tl,{chat: middleChat, speed: 10, pause: 350 })
-    //    }, "<")
-    // .fromTo(container, { y: "10%", }, {y: "0",})
-    // .fromTo(bottomChat, {
-    //     autoAlpha: 0
-    // }, {
-    //     autoAlpha: 1, 
-    //     onComplete: () => tl.pause(),
-    //     onStart: () => startTypeWriter(tl,{chat: bottomChat, speed: 17, pause: 350 })
-    //    }, "<")
-    // .fromTo([topChat, middleChat, bottomChat], {transformOrigin: "left", scale: 1}, {scale: 0.5, autoAlpha: 0, ease: Power3.easeInOut, duration: 1.5}, "+=1")
+    .fromTo(container, {y: "20%"}, {y: "10%"})
+    .fromTo(middleChat, {
+        autoAlpha: 0
+    }, {
+        autoAlpha: 1, 
+        onComplete: () => tl.pause(),
+        onStart: () => startTypeWriter(tl,{dialogue: dialogueTwo, chat: middleChat, speed: 10, pause: 350 })
+       }, "<")
+    .fromTo(container, { y: "10%", }, {y: "0",})
+    .fromTo(bottomChat, {
+        autoAlpha: 0
+    }, {
+        autoAlpha: 1, 
+        onComplete: () => tl.pause(),
+        onStart: () => startTypeWriter(tl,{dialogue: dialogueThree, chat: bottomChat, speed: 17, pause: 350 })
+       }, "<")
+    .fromTo(container, {
+        transformOrigin: "center", 
+        scale: 1,
+        autoAlpha: 1
+    }, {
+        scale: 0.5, 
+        autoAlpha: 0, 
+        ease: Power3.easeInOut, 
+        duration: 1.5,
+    }, "+=1");
 
     function startTypeWriter(tl, config) {
-        config.chat.style.minHeight = config.chat.scrollHeight + "px"; // <= Add min height to the chat container
-
-        const text = config.chat.textContent.trim();
-        let tw = new Typewriter(config.chat,{
-            delay: config.speed,
-        })
-
-        tw.pauseFor(config.pause)
-        .typeString(text)
+        config.chat.firstElementChild.textContent = null;
+        config.dialogue.pauseFor(config.pause)
+        .typeString(config.chat.getAttribute("data-text"))
         .start()
         .callFunction(() => {
+            tl.play();
             hideTypewriterCursor(config.chat);
         });
-
     }
 }
